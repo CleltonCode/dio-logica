dcf963850704b2e47895d92d681354715c36cc58
Clelton Henrique <160769903+CleltonCode@users.noreply.github.com>
2024-11-27 13:19:09 -0300

    Update index.js - inclusao desafio 2

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
- function obterDados() {
+function obterDados() {
+
     let nome = prompt("Por favor, insira o nome do personagem:");
-    let xp = prompt("Por favor entre com o nivle d XP do personagem: ");
-    
+    let xp = prompt("Por favor, entre com o nivle d XP do personagem: ");
+
+
     if (xp <= 1000) {
-        nivel = "Ferro";
+        experiencia = "Ferro";
     } else if (xp > 1000 && xp <= 2000) {
-        nivel = "Bronze";
+        experiencia = "Bronze";
     } else if (xp > 2000 && xp <= 5000) {
-        nivel = "Prata";
+        experiencia = "Prata";
     } else if (xp > 5000 && xp <= 7000) {
-        nivel = "Ouro";
-    } else if (xp > 7000 && xp <= 8000) {
-        nivel = "Platina";
-    } else if (xp > 8000 && xp <= 9000) {
-        nivel = "Ascendente";
-    } else if (xp > 9000 && xp <= 10000) {
-        nivel = "Imortal";
-    } else if (xp > 10000) {
-        nivel = "Radiante";
-    } else {
-        nivel = "Nível Inválido";
+        experiencia = "Ouro";
+        } else if (xp > 7000 && xp <= 8000) {
+            experiencia = "Platina";
+        } else if (xp > 8000 && xp <= 9000) {
+            experiencia = "Ascendente";
+        } else if (xp > 9000 && xp <= 10000) {
+            experiencia = "Imortal";
+        } else if (xp > 10000) {
+        experiencia = "Radiante";
+        }
+
+        
+        var vitorias = prompt("Por favor, insira o número de vitórias do personagem:");
+        let derrotas = prompt("Por favor, insira o número de derrotas do personagem:");
+        let total = 0;
+
+        if (vitorias.length <= 0 ) {
+        vitorias = 0;
+    }
+        
+    if (derrotas.length <= 0 ) {
+        derrotas = 0;
     }
 
-    if (nome && xp) {
-        let nomeProcessado = "O Herói de nome - " + nome + "\n esta no nível - " + nivel;
+    total = vitorias - derrotas;
+        
+    if (total <= 10) {
+        nivel = "Ferro";
+    } else if (total > 11 && total <= 20) {
+        nivel = "Bronze";
+    } else if (total > 21 && total <= 50) {
+        nivel = "Prata";
+    } else if (total > 51 && total <= 80) {
+        nivel = "Ouro";
+    } else if (total > 81 && total <= 90) {
+        nivel = "Diamante";
+    } else if (total > 91 && total <= 100) {
+        nivel = "Lendário";
+    } else if (total > 101) {
+        nivel = "Imortal";
+    }
+
+    if (nome && xp ) {
+        let nomeProcessado = "O Herói de nome - " + nome + "\nExperiência - " + nivel +
+        "\nSaldo de vitorias - " + total + "\nEsta no Nível - " + nivel;
         document.getElementById("resultado").innerText = nomeProcessado;
     } else {
         document.getElementById("resultado").innerText = "Nome e ou XP não informado.";
     }
 
-            
 }
