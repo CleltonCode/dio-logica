95ae10a201f6bb344aca5c900225d6662f516c82
Clelton Henrique <160769903+CleltonCode@users.noreply.github.com>
2024-11-27 16:06:29 -0300

    Classe heroi.js
    Classe Heroi

--- heroi.js
+++ heroi.js
@@ -1,1 +1,42 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        switch (this.tipo.toLowerCase()) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'realizou um ataque desconhecido'; 
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+
+const mago = new Heroi('Merlin', 250, 'mago');
+const guerreiro = new Heroi('Conan', 30, 'guerreiro');
+const monge = new Heroi('Shaolin', 50, 'monge');
+const ninja = new Heroi('Hattori', 20, 'ninja');
+
+mago.atacar();     
+guerreiro.atacar();
+monge.atacar();    
+ninja.atacar();    
 
